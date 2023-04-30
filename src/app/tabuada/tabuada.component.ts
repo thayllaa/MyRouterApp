b789d481e7365235837d8b7551c16446477cd69f
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-30 17:33:09 -0300

    1 file modified, 6 deleted

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabuada.component.css']
 })
 export class TabuadaComponent implements OnInit {
+  n: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  getNumbers() {
+    let arr = [];
+    for (let i = 1; i <= this.n; i++) {
+      arr.push(Math.trunc(100 * Math.random()));
+    }
+    return arr;
   }
 
+  multiplicar() {
+    let arr = [];
+    for (let i = 1; i <= 10; i++) {
+      arr.push(this.n * i);
+    }
+    return arr;
+  }
 }
