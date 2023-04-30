863167874763daa302c34a2563dfd43e4797b886
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-30 17:57:59 -0300

    1 file modified

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabuada',
   templateUrl: './tabuada.component.html',
-  styleUrls: ['./tabuada.component.css']
+  styleUrls: ['./tabuada.component.css'],
 
 })
 export class TabuadaComponent implements OnInit {
