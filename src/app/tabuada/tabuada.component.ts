5fe2a0f8fb3e1b6250894ccc3195f7f996a46f3d
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-30 18:25:19 -0300

    2 files modified

--- src/app/tabuada/tabuada.component.ts
+++ src/app/tabuada/tabuada.component.ts
@@ -1,38 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tabuada',
   templateUrl: './tabuada.component.html',
-  styleUrls: ['./tabuada.component.css'],
+  styleUrls: ['./tabuada.component.css']
 })
 
-export class TabuadaComponent implements OnInit {
-  number: number = 1;
+export class TabuadaComponent {
+  numero: number;
+  tabuada: any[];
 
-  constructor(private route: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.route.paramMap.subscribe((params) => {
-      this.number = Number(params.get('number'));
-    });
-  }
-
-  getNumbers() {
-    let arr = [];
-    for (let i = 1; i <= this.number; i++) {
-      arr.push(Math.trunc(100 * Math.random()));
-    }
-    return arr;
-  }
-
-  multiplicar() {
-    let arr = [];
+  gerarTabuada() {
+    this.tabuada = [];
     for (let i = 1; i <= 10; i++) {
-      arr.push(this.number * i);
+      this.tabuada.push({
+        operacao: `${this.numero} x ${i}`,
+        resultado: this.numero * i
+      });
     }
-    return arr;
   }
 }
