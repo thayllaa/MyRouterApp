b71130fdd7e5ac94dbc534885696b7bfafa07670
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-30 17:01:04 -0300

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { RootComponent } from './root/root.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { MultTableComponent } from './mult-table/mult-table.component';
 import { MultTableDetailsComponent } from './mult-table-details/mult-table-details.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { ThayllaComponent } from './thaylla/thaylla.component';
 
 @NgModule({
   imports: [
@@ -15,7 +15,7 @@ import { RouterModule } from '@angular/router';
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'about', component: AboutComponent },
+      { path: 'thaylla', component: ThayllaComponent },
       { path: 'mult-table', component: MultTableComponent },
       { path: 'mult-table/:n', component: MultTableDetailsComponent },
     ]),
@@ -24,7 +24,7 @@ import { RouterModule } from '@angular/router';
     RootComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent,
+    ThayllaComponent,
     MultTableComponent,
     MultTableDetailsComponent,
   ],
---
+++ src/app/thaylla/thaylla.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-thaylla',
+  templateUrl: './thaylla.component.html',
+  styleUrls: ['./thaylla.component.css'],
+})
+export class ThayllaComponent implements OnInit {
+  date: Date = new Date();
+
+  constructor() {}
+
+  ngOnInit() {}
+}
