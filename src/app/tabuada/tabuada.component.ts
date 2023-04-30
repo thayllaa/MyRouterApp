5258bcff7df38af4ebd5922cac85613b94b8a1de
thayllaa <100003483+thayllaa@users.noreply.github.com>
2023-04-30 17:08:15 -0300

    1 file modified, 4 added

---
+++ src/app/tabuada/tabuada.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabuada',
+  templateUrl: './tabuada.component.html',
+  styleUrls: ['./tabuada.component.css']
+})
+export class TabuadaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
